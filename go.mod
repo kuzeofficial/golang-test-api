d565ebedaeb5fca8e1f42cb78614a7ba8018175b
Cristian Fonseca <cfonsecacomas@gmail.com>
2022-08-12 02:13:37 +0100

    Created client and client of Fiber

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/deta/deta-go v1.0.0 // indirect
 	github.com/gofiber/fiber/v2 v2.36.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
-	fmt.Println("Hello, world.")
+    app := fiber.New()
+		app.Use(cors.New(cors.Config{
+			AllowOrigins: "*",
+		}))
+    // app.Get("/", helloWorldHandler)
+		app.Get("/users", userHandler)
+		app.Static("/", "./client/dist")
+    app.Listen(":3000")
 }
+
+func userHandler(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"name": "John Doe",
+			"age":  42,
+		})
+}
+
+// func helloWorldHandler(c *fiber.Ctx) error {
+// 	return c.SendString("Hello, World 👋!")
+// }
